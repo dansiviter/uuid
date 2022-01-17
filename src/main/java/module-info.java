17f46f5863b0dbd9756d7dc811a1d85c5430b901
dansiviter <dansiviter@gmail.com>
2022-01-17 08:51:57 +0000

    Fixing JavaDoc generation (#5)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,6 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+ /**
+	* UUID Generation.
+  */
 module uk.dansiviter.uuid {
 	exports uk.dansiviter.uuid;
 }
